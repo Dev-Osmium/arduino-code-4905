49f81337a762a9ca8c2638a148b7fa8c3f3fc841
Otmas <Oatmas64134@gmail.com>
2017-04-13 20:20:14 -0400

    Add main file to contain 'modes'

---
+++ _4905LEDSignMain/_4905LEDSignMain.ino
@@ -0,0 +1,13 @@
+#include <Adafruit_NeoPixel.h>
+int i = 0;
+int j = 0;
+int a = 0;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(20, 6, NEO_GRB + NEO_KHZ800);
